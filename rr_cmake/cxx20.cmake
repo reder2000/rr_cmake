3bffa458979e7eaeafd77f03404058ccc4bb7c25
Reder <rouge.richard@free.fr>
2021-04-17 18:49:52 +0200

    cxx20 update

--- rr_cmake/cxx20.cmake
+++ rr_cmake/cxx20.cmake
@@ -1,6 +1,13 @@
 set(CMAKE_CXX_STANDARD 20)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
+
+add_compile_options($<$<CXX_COMPILER_ID:MSVC>:/MP>)
+add_compile_options($<$<CXX_COMPILER_ID:MSVC>:/Zc:preprocessor>)
+add_compile_options($<$<CXX_COMPILER_ID:MSVC>:/wd5105>)
+
+
 if (MSVC)
-add_compile_options(/Zc:preprocessor /MP)
+add_definitions(-DNOMINMAX)
+add_definitions(-DWIN32_LEAN_AND_MEAN)
 endif ()
 
