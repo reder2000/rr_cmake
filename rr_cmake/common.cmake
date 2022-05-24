0cac4eb6864df7cf8d799907cf536d1f8119199c
Richard Rouge <rouge.richard@free.fr>
2022-05-24 17:00:34 +0200

    git_include instead of lengthy fetch_content

--- rr_cmake/common.cmake
+++ rr_cmake/common.cmake
@@ -1,3 +1,4 @@
 include(${CMAKE_CURRENT_LIST_DIR}/cxx20.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/FindVcpkInstall.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/visual_source_group.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/git_include.cmake)
---
+++ rr_cmake/git_include.cmake
@@ -0,0 +1,20 @@
+include(FetchContent)
+
+function (git_include package_name git_repository ) # optional include_dir
+	FetchContent_Declare(
+	  ${package_name}
+	  GIT_REPOSITORY ${git_repository}
+	  GIT_TAG origin/master
+	  GIT_REMOTE_UPDATE_STRATEGY REBASE_CHECKOUT
+	  GIT_PROGRESS TRUE
+	  GIT_SHALLOW TRUE
+	  LOG_UPDATE TRUE
+	)
+	set(${package_name}_BUILD_TESTS OFF)
+	FetchContent_MakeAvailable(${package_name})
+	if ("${ARGV2}" STREQUAL "")
+		include_directories(${${package_name}_SOURCE_DIR})
+	else ()
+		include_directories(${${package_name}_SOURCE_DIR}/${ARGV2})
+	endif ()
+endfunction()
