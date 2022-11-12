c333a7c3be28af3b0b06d2079a00a78faf73e5d1
reder2000 <rouge.richard@free.fr>
2022-11-12 16:46:08 +0100

    branh selection

--- rr_cmake/git_include.cmake
+++ rr_cmake/git_include.cmake
@@ -1,6 +1,6 @@
 include(FetchContent)
 
-function (git_include package_name git_repository ) # optional include_dir
+function (old_²git_include package_name git_repository ) # optional include_dir
 	FetchContent_Declare(
 	  ${package_name}
 	  GIT_REPOSITORY ${git_repository}
@@ -19,4 +19,52 @@ function (git_include package_name git_repository ) # optional include_dir
 	endif ()
 	# for config
 	include_directories(${${package_name}_BINARY_DIR})
+endfunction()
+
+# git_include(my_package https://mygithub/exmaple.git BRANCH dev SOURCEDIR mydir)
+function(git_include package_name git_repository)
+    set(prefix GI)
+    #set(flags IS_ASCII IS_UNICODE)
+    set(singleValues BRANCH SOURCEDIR)
+    #set(multiValues SOURCES RES)
+
+    include(CMakeParseArguments)
+
+    cmake_parse_arguments(PARSE_ARGV 2 ${prefix}
+                     "${flags}"
+                     "${singleValues}"
+                     "${multiValues}"
+	)
+
+	if ("${GI_BRANCH}" STREQUAL "")
+	set(GI_BRANCH master)
+	endif()
+
+#    message(" GI_BRANCH: ${GI_BRANCH}")
+#    message(" GI_SOURCEDIR: ${GI_SOURCEDIR}")
+#    #message(" DEMO_SOURCES: ${DEMO_SOURCES}")
+#    #message(" DEMO_RES: ${DEMO_RES}")
+
+#    message(" named arg package_name : ${package_name}")
+#    message(" named arg  git_repository: ${git_repository}")
+
+		FetchContent_Declare(
+	  ${package_name}
+	  GIT_REPOSITORY ${git_repository}
+	  GIT_TAG origin/${GI_BRANCH}
+	  GIT_REMOTE_UPDATE_STRATEGY REBASE_CHECKOUT
+	  GIT_PROGRESS TRUE
+	  GIT_SHALLOW TRUE
+	  LOG_UPDATE TRUE
+	)
+	set(${package_name}_BUILD_TESTS OFF)
+	FetchContent_MakeAvailable(${package_name})
+	if ("${GI_SOURCEDIR}" STREQUAL "")
+		include_directories(${${package_name}_SOURCE_DIR})
+	else ()
+		include_directories(${${package_name}_SOURCE_DIR}/${GI_SOURCEDIR})
+	endif ()
+	# for config
+	include_directories(${${package_name}_BINARY_DIR})
+
 endfunction()
